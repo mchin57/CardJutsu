696c6ad07325a67b7c920ee0a4cee8b62b64d950
mc1038 <mc1038a@student.american.edu>
2022-03-10 20:41:27 +0000

    Still haven't fixed random issue, but fully playable. Moved all the classes and functions into different files. May work on GUI next,

---
+++ cmake-build-debug/CMakeFiles/CardJutsu.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/57tuc/CLionProjects/CardJutsu/Deck.cpp" "CMakeFiles/CardJutsu.dir/Deck.cpp.obj" "gcc" "CMakeFiles/CardJutsu.dir/Deck.cpp.obj.d"
+  "C:/Users/57tuc/CLionProjects/CardJutsu/card.cpp" "CMakeFiles/CardJutsu.dir/card.cpp.obj" "gcc" "CMakeFiles/CardJutsu.dir/card.cpp.obj.d"
+  "C:/Users/57tuc/CLionProjects/CardJutsu/hand.cpp" "CMakeFiles/CardJutsu.dir/hand.cpp.obj" "gcc" "CMakeFiles/CardJutsu.dir/hand.cpp.obj.d"
+  "C:/Users/57tuc/CLionProjects/CardJutsu/main.cpp" "CMakeFiles/CardJutsu.dir/main.cpp.obj" "gcc" "CMakeFiles/CardJutsu.dir/main.cpp.obj.d"
+  "C:/Users/57tuc/CLionProjects/CardJutsu/utils.cpp" "CMakeFiles/CardJutsu.dir/utils.cpp.obj" "gcc" "CMakeFiles/CardJutsu.dir/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CardJutsu.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CardJutsu.dir/Deck.cpp.obj"
+  "CMakeFiles/CardJutsu.dir/Deck.cpp.obj.d"
+  "CMakeFiles/CardJutsu.dir/card.cpp.obj"
+  "CMakeFiles/CardJutsu.dir/card.cpp.obj.d"
+  "CMakeFiles/CardJutsu.dir/hand.cpp.obj"
+  "CMakeFiles/CardJutsu.dir/hand.cpp.obj.d"
+  "CMakeFiles/CardJutsu.dir/main.cpp.obj"
+  "CMakeFiles/CardJutsu.dir/main.cpp.obj.d"
+  "CMakeFiles/CardJutsu.dir/utils.cpp.obj"
+  "CMakeFiles/CardJutsu.dir/utils.cpp.obj.d"
+  "CardJutsu.exe"
+  "CardJutsu.exe.manifest"
+  "CardJutsu.pdb"
+  "libCardJutsu.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CardJutsu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
